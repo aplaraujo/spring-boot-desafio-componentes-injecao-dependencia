a84ef973d3cf56b0d45b4f2c6ccea4d9fcf61727
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-25 20:19:57 -0300

    Project creation

---
+++ src/main/java/com/example/spring_boot_desafio_componentes_injecao_dependencia/SpringBootDesafioComponentesInjecaoDependenciaApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_desafio_componentes_injecao_dependencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDesafioComponentesInjecaoDependenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDesafioComponentesInjecaoDependenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_desafio_componentes_injecao_dependencia/SpringBootDesafioComponentesInjecaoDependenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_desafio_componentes_injecao_dependencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDesafioComponentesInjecaoDependenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
